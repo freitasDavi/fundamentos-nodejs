9a69f8ffb2a09fcc3bb7b25c678ab9f9d0a274f2
Davi <davi.frrs@gmail.com>
2022-10-05 17:31:47 -0300

    fix: refactor implementations location

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ListCategoriesUseCase } from "./ListaCategoriesUseCase";
 import { ListCategoriesController } from "./ListCategoriesController";
 
