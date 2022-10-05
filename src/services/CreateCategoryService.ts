e8f3b13256a2918258b0c191a2d6f161384f1ead
Davi <davi.frrs@gmail.com>
2022-10-05 16:49:42 -0300

    feat: Principio da Responsabilidade Unica SRP

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,40 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+	name: string;
+	description: string;
+}
+
+/*
+	[x] Definir o tipo de retorno
+	[x] Alterar o retorno de erro
+	[x] Acessar o repositório
+	[x] Retornar algo?
+*/
+
+
+class CreateCategoryService {
+
+	// private categoriesRepository: CategoriesRepository;
+
+	// constructor (private categoriesRepository: CategoriesRepository) {
+	// 	this.categoriesRepository = categoriesRepository;
+	// }
+
+	// Esse de baixo é a mesma coisa que o de cima
+	constructor (private categoriesRepository: CategoriesRepository) {}
+
+	execute ({ name, description }: IRequest): void {
+		const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+		if (categoryAlreadyExists) {
+			throw new Error("Category already exists");
+		}
+	
+		this.categoriesRepository.create({ name, description });
+	
+	}
+
+}
+
+export { CreateCategoryService };
