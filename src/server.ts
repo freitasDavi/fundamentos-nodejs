e906513ac8f7b0486fade886af5d6b708c190113
Davi <davi.frrs@gmail.com>
2022-10-05 16:33:34 -0300

    feat: added repositories

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { categoriesRotues } from "./routes/categories.routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/categories", categoriesRotues);
+app.use("/categories", categoriesRoutes);
 
 app.get("/", (request, response) => response.send("hello world"));
 
